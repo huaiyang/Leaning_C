7e0aba508979d2e09254db2233c2dceb6b95ec39
Bill Wang <bill.wxx@gmail.com>
2023-07-17 05:59:05 +0800

    nothing.c

---
+++ chapter2/nothing.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: Bill Wang bill.wxx@gmail.com
+ * @Date: 2023-07-17 05:57:46
+ * @LastEditors: Bill Wang bill.wxx@gmail.com
+ * @LastEditTime: 2023-07-17 05:57:59
+ * @FilePath: /Leaning_C/chapter2/nothing.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+int main()
+{
+    return 0;
+}
