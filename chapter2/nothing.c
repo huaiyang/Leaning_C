9011ded449cf0abb4213ca9cc771a3fa831dbd1e
bill <bill.wxx@gmail.com>
2023-07-17 09:16:34 +0800

    delete 1 line

--- chapter2/nothing.c
+++ chapter2/nothing.c
@@ -1,9 +1,8 @@
 /*
  * @Author: Bill Wang bill.wxx@gmail.com
  * @Date: 2023-07-17 05:57:46
- * @LastEditors: Bill Wang bill.wxx@gmail.com
- * @LastEditTime: 2023-07-17 05:57:59
- * @FilePath: /Leaning_C/chapter2/nothing.c
+ * @LastEditors: bill bill.wxx@gmail.com
+ * @LastEditTime: 2023-07-17 09:16:22
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 int main()
